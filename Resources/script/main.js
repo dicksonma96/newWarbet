3f6db88ff3cd5c520c4db0e2d130babb991edb9d
Dickson <dickson.ma@claritas.asia>
2024-11-09 02:05:53 +0800

    update

--- Resources/script/main.js
+++ Resources/script/main.js
@@ -2,10 +2,12 @@ import { Language } from "./language.js";
 import { InitSwiper } from "./homeBanner.js";
 import SlotLobby from "./slotLobby.js";
 import FishingLobby from "./fishingLobby.js";
+import ShowToggle from "./showToggle.js";
 
 $(document).ready(function () {
   Language();
   InitSwiper();
   SlotLobby();
   FishingLobby();
+  ShowToggle();
 });
