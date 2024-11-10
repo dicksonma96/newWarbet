08fa49ad6530fa0f88c0f40ee97603f7e042a67e
Dickson <dickson.ma@claritas.asia>
2024-11-11 00:37:49 +0800

    udpate;

--- Resources/script/customDropdown.js
+++ Resources/script/customDropdown.js
@@ -1,8 +1,14 @@
-export function Language() {
+export default function () {
   $(".language_picker").click(function (event) {
     event.stopPropagation();
     $(this).parents(".language").toggleClass("language_show");
   });
+
+  $(".user_panel").click(function (event) {
+    event.stopPropagation();
+    $(this).toggleClass("user_menu_show");
+  });
+
   $(document).click(function (event) {
     if (!$(event.target).closest(".language").length) {
       $(".language").removeClass("language_show");
--- Resources/script/main.js
+++ Resources/script/main.js
@@ -1,13 +1,19 @@
-import { Language } from "./language.js";
+import CustomDropdown from "./customDropdown.js";
 import { InitSwiper } from "./homeBanner.js";
 import SlotLobby from "./slotLobby.js";
 import FishingLobby from "./fishingLobby.js";
 import ShowToggle from "./showToggle.js";
+import Timer from "./timer.js";
+import ProgressBar from "./progressbar.js";
+import Fund from "./fund.js";
 
 $(document).ready(function () {
-  Language();
+  CustomDropdown();
   InitSwiper();
   SlotLobby();
   FishingLobby();
   ShowToggle();
+  Timer();
+  ProgressBar();
+  Fund();
 });
